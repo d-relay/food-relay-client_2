c1295356b7ec3dd53cf6b70ef2748c43953e69d2
codefactor-io <support@codefactor.io>
2020-09-29 13:41:00 +0000

    [CodeFactor] Apply fixes

--- postcss.config.js
+++ postcss.config.js
@@ -4,7 +4,7 @@ const autoprefixer = require('autoprefixer')({ overrideBrowserslist: ['last 2 ve
 
 const purgecss = require("@fullhuman/postcss-purgecss")({
     content: ["./src/**/*.svelte", "./src/**/*.html"],
-    defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+    defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || []
 });
 
 const production = process.env.NODE_ENV === 'production';
